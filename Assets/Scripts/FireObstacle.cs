f2cebbcf9cafda2fd560e7ae9f5fdfe26b35e722
Avineesh Kompella <AvineeshKom@gmail.com>
2018-06-04 13:01:21 -0700

    FireObstacle updates
    FireObstacles activate only when they're in view of the camera. Also changed the timing of the FireObstacles.

--- Assets/Scripts/FireObstacle.cs
+++ Assets/Scripts/FireObstacle.cs
@@ -5,48 +5,78 @@ using UnityEngine;
 public class FireObstacle : MonoBehaviour {
 
     public GameObject fire;
+    public int height;
+    public bool isOdd;
+
 
 	// Use this for initialization
 	void Start () {
 
 	}
 
-    IEnumerator Fire() {
+    IEnumerator Fire()
+    {
         // just a "stopwatch" for debugging purposes.
         print((int)Time.time);
-        yield return new WaitForSeconds(5);
-
-        //for every other 5 seconds, clone the FireObstacle
-        if ((int)Time.time % 5 == 0 && (int)Time.time % 2 != 0) 
+        yield return new WaitForSeconds(1);
+        int count = 0;
+        GameObject[] fires = GameObject.FindGameObjectsWithTag(fire.tag);
+        foreach (GameObject i in fires)
         {
-            if (GameObject.FindGameObjectsWithTag("Fire").Length < 10)
+            if (i.name == fire.name + "(Clone)")
             {
-                GameObject.Instantiate(fire, transform.position, transform.rotation); 
+                count++;
             }
         }
-        //for every other other 5 seconds(idfk the proper term for it; shut up), destroy the clones
-        else if ((int)Time.time % 5 == 0 && (int)Time.time % 2 == 0)
+        if (fire.GetComponent<Renderer>().isVisible)
         {
-            GameObject[] fires = GameObject.FindGameObjectsWithTag("Fire");
+            //depending on isOdd's value, clone the FireObstacle
+            if ((isOdd == false && (int)Time.time % 2 == 0) || (isOdd == true && (int)Time.time % 2 != 0))
+            {
+                if (count < height)
+                {
+                    Vector3 y = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                    for (int i = 1; i <= height; i++)
+                    {
+                        y.y = y.y + (float)0.1;
+                        GameObject.Instantiate(fire, y, transform.rotation);
+                    }
+                }
+            }
+            //destroy the clones if the parity of the time is contradictory to isOdd
+            else
+            {
+                foreach (GameObject i in fires)
+                {
+                    if (i.name == fire.name + "(Clone)")
+                    {
+                        GameObject.Destroy(i);
+                    }
+                }
+            }
+        }
+        else
+        {
             foreach (GameObject i in fires)
             {
-                if (i.name == "FireObstacle(Clone)")
+                if (i.name == fire.name + "(Clone)")
                 {
                     GameObject.Destroy(i);
                 }
             }
-        } 
-    }
-
-    //if the player hits the fire, he/she is destroyed. I'm assuming the Destroy method will be replaced by whatever's written in the health script.
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if(col.gameObject.name == "Player")
-        {
-            Destroy(col.gameObject);
         }
     }
 
+    //if the player hits the fire, he/she is destroyed. I'm assuming the Destroy method will be replaced by whatever's written in the health script.
+    
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            col.gameObject.GetComponent<Player>().playerHealth = 0;
+        }
+    }
+    
     // Update is called once per frame
     private void Update()
     {
